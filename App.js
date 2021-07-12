f0e5d12f279031a54857b61cd237d7390b3d19ec
Ekpenyong <okpo.ekpenyong@gmail.com>
2021-07-12 08:14:04 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,105 @@
+import {SafeAreaView, StatusBar, LogBox} from 'react-native';
+import React, {useEffect} from 'react';
+import CreateRootNavigator from './src/index';
+import {StateProvider} from './src/services/State/State';
+import {initialState} from './src/services/State/InitialState';
+import {reducer, actions} from './src/services/State/Reducer';
+import {useStateValue} from './src/services/State/State';
+import ModalActivityIndicator from './src/components/ModalActivityIndicator';
+import AppAlert from './src/components/AppAlert';
+import {theme} from './src/services/Common/theme';
+import {getUserInfo} from './src/services/DataManager';
+import { store } from './src/store/store.js'
+import { getWeb3_} from './src/web3/getWeb3'
+import { Provider } from 'react-redux'
+import { persistStore } from "redux-persist"
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store);
+getWeb3_.catch(
+  err => console.warn('Error in web3 initialization.', err)
+)
+
+const RootNavigator = () => {
+  useEffect(() => {
+      checkStatus();
+  }, []);
+
+  const [{progressSettings, alertSettings}, dispatch] = useStateValue();
+  const {show = false} = progressSettings || {};
+  const {settings} = alertSettings || {};
+
+  const checkStatus = async () => {
+    try {
+      const userInfo = await getUserInfo();
+      if (userInfo && userInfo.id) {
+        dispatch({
+          type: actions.SET_USER,
+          user: userInfo,
+        });
+      } else {
+        dispatch({
+          type: actions.SET_USER,
+          user: '',
+        });
+      }
+      // eslint-disable-next-line no-empty
+    } catch (err) {}
+  };
+
+  const getAlertSettings = () => {
+    const onConfirmPressed =
+        settings && settings.onConfirmPressed
+          ? settings.onConfirmPressed
+          : () => {},
+      onCancelPressed =
+        settings && settings.onCancelPressed
+          ? settings.onCancelPressed
+          : () => {};
+    return {
+      ...settings,
+      onConfirmPressed: () => {
+        dispatch({
+          type: actions.SET_ALERT_SETTINGS,
+          alertSettings: null,
+        });
+        onConfirmPressed();
+      },
+      onCancelPressed: () => {
+        dispatch({
+          type: actions.SET_ALERT_SETTINGS,
+          alertSettings: null,
+        });
+        onCancelPressed();
+      },
+    };
+  };
+
+  return (
+    <>
+      <SafeAreaView style={{flex: 0, backgroundColor: theme.APP_COLOR}} />
+      <SafeAreaView style={{flex: 1, backgroundColor: theme.COLORS.WHITE}}>
+        <StatusBar backgroundColor={theme.APP_COLOR} barStyle="dark-content" />
+        <AppAlert {...getAlertSettings()} />
+        <ModalActivityIndicator modalVisible={show} />
+        <CreateRootNavigator />
+      </SafeAreaView>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StateProvider initialState={initialState} reducer={reducer}>
+          <RootNavigator />
+        </StateProvider>
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default App;
+
+LogBox.ignoreAllLogs(true);
---
+++ walletEntry.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import './global'
+import './shim'
+import React, {Component} from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './src/WalletHome'
+import WalletSettings from './src/screens/WalletSettings'
+import WalletActions from './src/screens/WalletActions'
+
+
+
+const Stack = createStackNavigator()
+
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="WalletHome" component={Home} options={{ title: 'Wallet Settings' }} />
+        <Stack.Screen name="Add Liquidity" component={WalletActions} navigation={this.props.navigation} />
+        <Stack.Screen name="Wallet" component={WalletSettings} navigation={this.props.navigation} />
+      </Stack.Navigator>
+)
+  }
+}
